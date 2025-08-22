34937e7d433e4acb9d5cb680b11c0db35bd0cbef
Sebastian <ungureanusebastian2016@gmail.com>
2025-08-22 17:20:35 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-// Selectează toate linkurile din header
+// Smooth scroll pentru linkurile din header
 const links = document.querySelectorAll('header nav a');
 
 for (const link of links) {
   link.addEventListener('click', function(e) {
-    e.preventDefault(); // previne jump instant
+    e.preventDefault();
     const targetId = this.getAttribute('href').substring(1);
     const targetSection = document.getElementById(targetId);
 
